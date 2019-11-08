6f92c7cb6f63f986c008c3d5e647506a73d043ec
satoruosada <satoru.5714@gmail.com>
2019-11-08 13:26:46 +0900

    コミットした

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(function () {
+
+  // フロートヘッダーメニュー
+  var targetHeight = $('.js-float-menu-target').height();
+  $(window).on('scroll', function() {
+    $('.js-float-menu').toggleClass('float-active', $(this).scrollTop() > targetHeight);
+  });
+
+  //spメニュー
+  $('.js-toggle-sp-menu').on('click',function(){
+    $(this).toggleClass('active');
+    $('.js-toggle-sp-menu-target').toggleClass('active');
+  });
+});
+
+ $(function(){
+   $(window).scroll(function(){
+     $('.js-fadeIn').each(function(){
+       var targetElement = $(this).offset().top;
+       var scroll = $(window).scrollTop();
+       var windowHeight = $(window).height();
+       if(scroll > targetElement - windowHeight + 300){
+         $(this).css('opacity','1');
+         $(this).css('transform','translateY(0)');
+       }
+     });
+   });
+ });
+
+
+  $(function(){
+    $(window).scroll(function (){
+        $('.js-slideIn').each(function(){
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 300){
+                $(this).css('opacity','2');
+                $(this).css('transform','translateX(0)');
+        }
+      });
+    });
+  });
