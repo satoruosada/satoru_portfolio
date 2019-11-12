c53121266d5dfda3ab2133d792dc80a9bd79ec09
satoruosada <satoru.5714@gmail.com>
2019-11-12 13:27:15 +0900

    no message

---
+++ css/bundle.js
@@ -0,0 +1,46 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+$(function () {
+
+  // フロートヘッダーメニュー
+  var targetHeight = $('.js-float-menu-target').height();
+  $(window).on('scroll', function() {
+    $('.js-float-menu').toggleClass('float-active', $(this).scrollTop() > targetHeight);
+  });
+
+  //spメニュー
+  $('.js-toggle-sp-menu').on('click',function(){
+    $(this).toggleClass('active');
+    $('.js-toggle-sp-menu-target').toggleClass('active');
+  });
+});
+
+ $(function(){
+   $(window).scroll(function(){
+     $('.js-fadeIn').each(function(){
+       var targetElement = $(this).offset().top;
+       var scroll = $(window).scrollTop();
+       var windowHeight = $(window).height();
+       if(scroll > targetElement - windowHeight + 300){
+         $(this).css('opacity','1');
+         $(this).css('transform','translateY(0)');
+       }
+     });
+   });
+ });
+
+
+  $(function(){
+    $(window).scroll(function (){
+        $('.js-slideIn').each(function(){
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 300){
+                $(this).css('opacity','2');
+                $(this).css('transform','translateX(0)');
+        }
+      });
+    });
+  });
+
+},{}]},{},[1]);
