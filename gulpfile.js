08a09e9c73934873471af2bc80b054cef27676cf
satoruosada <satoru.5714@gmail.com>
2019-11-14 16:39:37 +0900

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 //plug-in
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserify = require('browserify');
 var browserSync = require('browser-sync').create();
 var source = require('vinyl-source-stream');
+
+// sassとcssの保存先
+gulp.task('sass', function(){
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('./css/'));
+});
+
+//自動監視
+gulp.task('sass-watch', ['sass'], function(){
+    var watcher = gulp.watch('./sass/**/*.scss', ['sass']);
+    watcher.on('change', function(event) {
+    });
+});
+
+gulp.task('default', ['sass-watch']);
 
 
 // gulpタスクの作成
