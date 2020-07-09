06d5262f74c6f4c3b2d0a38b1f889e409911c039
satoruosada <satoru.5714@gmail.com>
2020-07-09 19:28:58 +0900

    コミットする

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,39 @@
 //plug-in
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var browserify = require('browserify');
 var browserSync = require('browser-sync').create();
 var source = require('vinyl-source-stream');
 
-// sassとcssの保存先
-gulp.task('sass', function(){
-    gulp.src('./sass/**/*.scss')
-        .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('./css/'));
-});
-
-//自動監視
-gulp.task('sass-watch', ['sass'], function(){
-    var watcher = gulp.watch('./sass/**/*.scss', ['sass']);
-    watcher.on('change', function(event) {
-    });
-});
-
-gulp.task('default', ['sass-watch']);
-
 
 // gulpタスクの作成
-gulp.task('build', function(){
+gulp.task('build', function(done){
   browserify({
     entries: ['src/app.js'] // ビルド元のファイルを指定
   }).bundle()
     .pipe(source('bundle.js')) // 出力ファイル名を指定
-    .pipe(gulp.dest('css/')); // 出力ディレクトリを指定
+    .pipe(gulp.dest('dist/')); // 出力ディレクトリを指定
+    done();
 });
-gulp.task('browser-sync', function() {
+
+gulp.task('browser-sync', function(done) {
   browserSync.init({
     server: {
       baseDir: "./", // 対象ディレクトリ
       index: "index.html" //indexファイル名
     }
   });
+  done();
 });
-gulp.task('bs-reload', function () {
+
+gulp.task('bs-reload', function (done) {
   browserSync.reload();
+  done();
 });
 
 // Gulpを使ったファイルの監視
-gulp.task('default', gulp.series(gulp.parallel('build', 'browser-sync'), function(){
-  gulp.watch('./src/*.js', ['build']);
-  gulp.watch("./*.html", ['bs-reload']);
-  gulp.watch("./css/*.+(js|css)", ['bs-reload']);
+gulp.task('default', gulp.series('build', 'browser-sync', function(done){
+  gulp.watch('./src/*.js', gulp.series('build'));
+  gulp.watch("./*.html", gulp.series('bs-reload'));
+  gulp.watch("./css/*.+(js|css)", gulp.series('bs-reload'));
+  done();
 }));
